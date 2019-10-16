7484a45b8ace0b58fcac79d4d2eb9bbad5d17653
Daniel Mordi <50272668+danielmordi@users.noreply.github.com>
2019-10-16 12:16:08 +0100

    Created database for admin, user and course

--- database/migrations/2019_10_16_101214_create_admin_table.php
+++ database/migrations/2019_10_16_101214_create_admin_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAdminTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admin', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('first_name');
             $table->string('other_name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('exprience');
-            $table->string('country');
-            $table->string('state');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -35,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admin');
     }
 }
