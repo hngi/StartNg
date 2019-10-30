f46fb028629a5f978dbbe4672e814ec2da85701a
Busari Abdullah <abdullah_busari@yahoo.com>
2019-10-30 19:42:57 +0100

    layout

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,58 +1,45 @@
 <nav class="navbar navbar-expand-lg navbar-custom bg-custom">
-        <div class="container pt-2 pb-2">
-            <a href="/" class="navbar-brand"><img
-                    src="https://res.cloudinary.com/sgnolebagabriel/image/upload/v1570873250/startng/Logo_1_ib5bjh.png"
-                    class="img-fluid" alt="logo" width="150px"></a>
-            <button class="navbar-toggler float-right custom-toggler" type="button" data-toggle="collapse"
-                data-target="#navbar9" style="color: #000;">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="navbar-collapse collapse" id="navbar9">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item mr-5">
-                        <a class="nav-link" href="{{route('about')}}">About Us</a>
+    <div class="container col-lg-12 pl-5 pr-3">
+        <a href="/" class="navbar-brand"><img class="img-fluid" src="https://res.cloudinary.com/sgnolebagabriel/image/upload/v1570873250/startng/Logo_1_ib5bjh.png"></a>
+        <button class="navbar-toggler float-right custom-toggler" type="button" data-toggle="collapse" data-target="#navbar9" style="color: #000;">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="navbar9">
+            <ul class="navbar-nav ml-auto pt-2">
+                <li class="nav-item pr-4">
+                    <a class="nav-link" href="{{route('about')}}">About Us</a>
+                </li>
+                <li class="nav-item mr-4">
+                    <a class="nav-link" href="{{route('course.index')}}">Courses</a>
+                </li>
+                <li class="nav-item pr-4">
+                    <a class="nav-link" href="{{route('hire')}}">Hire a Grad</a>
+                </li>
+                <li class="nav-item pr-4">
+                    <a class="nav-link" href="{{route('contact')}}">Contact Us</a>
+                </li>
+                @if(Auth::guest())
+                    <li class="nav-item mr-4">
+                        <a class="btn btn-custom nav-link pl-5 pr-5" href="{{route('register')}}" style="color: #fff;">Register</a>
                     </li>
-                    <li class="nav-item mr-5">
-                        <a class="nav-link" href="{{route('course.index')}}">Courses</a>
+                    <li class="nav-item mr-4">
+                        <a class="btn btn-custom nav-link pl-5 pr-5" href="{{route('login')}}" style="color: #fff;">Login</a>
                     </li>
-
-                    
-                    <li class="nav-item mr-5">
-                        <a class="nav-link" href="{{route('hire')}}">Hire A Grad</a>
+                @else
+                    <li class="nav-item mr-4">
+                        <a class="btn btn-custom nav-link pl-5 pr-5" href="#" style="color: #fff;">Dashboard</a>
                     </li>
-                    <li class="nav-item mr-5">
-                        <a class="nav-link" href="{{route('contact')}}">Contact Us</a>
-                    </li>
+                    <a class="btn btn-custom nav-link pl-5 pr-5" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
 
-                    @if(!Auth::guest())
-
-
-                        <li class="nav-item mr-5">
-                            <a class="btn btn-success nav-link px-5" href="/mycourses/{{\Illuminate\Support\Facades\Auth::user()->id}}" style="color: #fff;">My courses</a>
-                        </li>
-                    @endif
-
-                    @if(!Auth::guest())
-                        <a class="btn btn-success nav-link px-5" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    @endif
-
-                    @if(Auth::guest())
-                        <li class="nav-item mr-5">
-                            <a class="btn btn-success nav-link px-5" href="{{route('register')}}" style="color: #fff;">Register</a>
-                        </li>
-                        <li class="nav-item mr-5">
-                            <a class="btn btn-success nav-link px-5" href="{{route('login')}}" style="color: #fff;">Login</a>
-                        </li>
-                    @endif
-                </ul>
-            </div>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                @endif
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,161 +1,151 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0-11/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
-    <title>Contact Us</title>
+@section('style')
 
-    <style>
-        body {
-            font-family: Nunito;
-        }
+<style>
+    body {
+        font-family: Nunito;
+    }
 
-        /* Style for Navbar Starts */
+    /* Style for Navbar Starts */
 
-        .navbar-custom li a {
-            color: #000;
-        }
+    .navbar-custom li a {
+        color: #000;
+    }
 
-        .btn-success {
-            background-color: #2DCE89;
-            color: #fff;
-            border: thin solid #2dce89;
-        }
+    .btn-success {
+        background-color: #2DCE89;
+        color: #fff;
+        border: thin solid #2dce89;
+    }
 
-        .btn-success:hover {
-            background-color: rgb(2, 20, 12);
-            color: #fff;
-            border: thin solid rgb(2, 20, 12);
-        }
+    .btn-success:hover {
+        background-color: rgb(2, 20, 12);
+        color: #fff;
+        border: thin solid rgb(2, 20, 12);
+    }
 
-        .custom-toggler .navbar-toggler-icon {
-            background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(0,0,0, 0.7)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
-        }
+    .custom-toggler .navbar-toggler-icon {
+        background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(0,0,0, 0.7)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
+    }
 
-        .custom-toggler.navbar-toggler {
-            border-color: #000;
-        }
+    .custom-toggler.navbar-toggler {
+        border-color: #000;
+    }
 
-        /* Style for Navbar Ends */
-        /* Style for Footer Starts */
+    /* Style for Navbar Ends */
+    /* Style for Footer Starts */
 
-        * {
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            font-family: 'Nunito', sans-serif;
-        }
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Nunito', sans-serif;
+    }
 
-        a {
-            text-decoration: none;
-        }
+    a {
+        text-decoration: none;
+    }
 
-        .deep {
-            background: #3A0842;
-            color: #fff;
-        }
+    .deep {
+        background: #3A0842;
+        color: #fff;
+    }
 
-        .deep #slink {
-            background: #44CF6C;
-            border-radius: 10px;
-            color: white;
-            padding: 10px 70px;
-        }
+    .deep #slink {
+        background: #44CF6C;
+        border-radius: 10px;
+        color: white;
+        padding: 10px 70px;
+    }
 
-        .deep li a {
-            text-decoration: none;
-            color: #fff;
-            font-size: 16px;
-            line-height: 30px;
-        }
+    .deep li a {
+        text-decoration: none;
+        color: #fff;
+        font-size: 16px;
+        line-height: 30px;
+    }
 
-        .deep li {
-            list-style: none;
-        }
+    .deep li {
+        list-style: none;
+    }
 
-        .deep li a:hover {
-            text-decoration: none;
-        }
+    .deep li a:hover {
+        text-decoration: none;
+    }
 
-        a:hover {
-            text-decoration: none;
-        }
-        
+    a:hover {
+        text-decoration: none;
+    }
+    
 
-        .icons i {
-            font-size: 26px;
-            height: 50px;
-            width: 50px;
-            padding: 10px 20px;
-            color: white;
-        }
+    .icons i {
+        font-size: 26px;
+        height: 50px;
+        width: 50px;
+        padding: 10px 20px;
+        color: white;
+    }
 
-        .icons i.fa.fa-twitter:hover {
-            color: #00aced;
-        }
+    .icons i.fa.fa-twitter:hover {
+        color: #00aced;
+    }
 
-        .icons i.fa.fa-facebook:hover {
-            color: #3b5598;
-        }
+    .icons i.fa.fa-facebook:hover {
+        color: #3b5598;
+    }
 
-        .icons i.fa.fa-instagram:hover {
-            color: #ed3833;
-        }
+    .icons i.fa.fa-instagram:hover {
+        color: #ed3833;
+    }
 
-        /* Style for Footer Ends */
-        /* Body Style */
+    /* Style for Footer Ends */
+    /* Body Style */
 
-        .search {
-            background-color: #44CF6C;
-        }
+    .search {
+        background-color: #44CF6C;
+    }
 
-        .support {
-            background-image: url('https://res.cloudinary.com/sgnolebagabriel/image/upload/v1570910454/startng/Rectangle_66_iauqka.png');
-            background-position: center center;
-            background-repeat: no-repeat;
-            background-size: cover;
-        }
+    .support {
+        background-image: url('https://res.cloudinary.com/sgnolebagabriel/image/upload/v1570910454/startng/Rectangle_66_iauqka.png');
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    .contact {
+        width: 1200px;
+        margin-left: 25px;
+    }
+    @media screen and (max-width: 1024px) {
         .contact {
             width: 1200px;
-            margin-left: 25px;
+            margin-left: 10px;
         }
-        @media screen and (max-width: 1024px) {
-            .contact {
-                width: 1200px;
-                margin-left: 10px;
-            }
+    }
+    @media screen and (max-width: 768px) {
+        .contact {
+            width: 1200px;
+            margin-left: 10px;
         }
-        @media screen and (max-width: 768px) {
-            .contact {
-                width: 1200px;
-                margin-left: 10px;
-            }
+    }
+    @media screen and (max-width: 425px) {
+        .contact {
+            width: 1200px;
+            margin-left: -21px;
         }
-        @media screen and (max-width: 425px) {
-            .contact {
-                width: 1200px;
-                margin-left: -21px;
-            }
+    }
+    @media screen and (max-width: 375px) {
+        .contact {
+            width: 1200px;
+            margin-left: -15px;
         }
-        @media screen and (max-width: 375px) {
-            .contact {
-                width: 1200px;
-                margin-left: -15px;
-            }
-        }
-        /* Body Style Ends*/
-    </style>
-</head>
+    }
+    /* Body Style Ends*/
+</style>
+@endsection
 
-<body>
-    @include('inc.navbar')
-
+@section('content')
     <div class="container-fluid banner">
         <img src="https://res.cloudinary.com/sgnolebagabriel/image/upload/v1570908542/startng/Group_411_d0xdb8.png" class="img-fluid contact" height="100">
     </div>
@@ -226,13 +216,4 @@
 
     </div>
 
-    <!-- Footer -->
-    @include('inc.footer')
-    <!-- End of Footer -->
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
-</body>
-
-</html> 
+@endsection
