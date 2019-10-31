69f52199b0473c01d018f585e8a0117df6d517fd
realayo <gudmuv@gmail.com>
2019-10-31 20:21:17 +0100

    forgot password

--- config/app.php
+++ config/app.php
@@ -161,10 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
-        Spatie\Newsletter\NewsletterServiceProvider::class,
-
-
 
         /*
          * Package Service Providers...
@@ -178,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
 
     ],
 
@@ -229,9 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
-        'NewsLetter' => Spatie\Newsletter\NewsletterFacade::class
+
+
     ],
 
 ];
