9d01e7379524efa69ad6dc6228e0565e753177de
odun <biodun.agbolade@gmail.com>
2019-11-08 03:33:43 +0100

    UI and Responsiveness

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
     <link href="/css/modifiedstyles.css" rel="stylesheet" />
-    <title>aaa |</title>
+    <title>Start NG |</title>
 
     <style>
         body {
--- resources/views/course/courses.blade.php
+++ resources/views/course/courses.blade.php
@@ -1,12 +1,51 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('style')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0-11/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+    <title>Start NG | Courses</title>
 
 <style>
     body {
         font-family: Nunito;
     }
-    
+
+    a.nav-link:hover {
+      color: #2DCE89;
+    }
+    /* Style for Navbar Starts */
+    .navbar-custom li a {
+        color: #000;
+    }
+    .btn-success {
+        background-color: #2DCE89;
+        color: #fff;
+        border: thin solid #2dce89;
+    }
+    /* .btn-success:hover {
+        background-color: rgb(2, 20, 12);
+        color: #fff;
+        border: thin solid rgb(2, 20, 12);
+    } */
+    .btn-primary:hover {
+        background-color: var(--primary-color);
+        opacity: 0.85;
+    }
+    .btn-secondary {
+    background-color: #3A0842;
+    border-color: #3A0842;
+    color: #fff !important;
+    cursor: pointer;
+    }
+
     .body-banner {
         background-image: url('https://res.cloudinary.com/sgnolebagabriel/image/upload/v1570961662/startng/Group_420_naoymq.png');
         background-size: cover;
@@ -14,9 +53,10 @@
         background-position: center center;
     }
 </style>
-@endsection
+</head>
+<body>
 
-@section('content')
+@include('inc.navbar')
 <div class="container-fluid pt-5 pb-5 body-banner">
         <div class="col-md-12 mt-5 pt-5">
             <h4 class="text-center font-weight-bold mt-5 pt-5" style="color: #fff;">Our Courses</h4>
@@ -98,7 +138,7 @@
 
     <div class="container-fluid phones" style="background-color: #2A2B2A;">
         <div class="row align-items-center">
-            <div class="col-6">
+            <div class="col-lg-6 col-sm-12 mt-4 ">
                 <div class="col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-3 text-white" style="font-size: 120%;">
                     <h4>Benefits</h4>
                     <li style="list-style-image: url('https://res.cloudinary.com/sgnolebagabriel/image/upload/v1572644348/startng/Vector-2_xigbuh.png');">
@@ -120,7 +160,7 @@
                     @endif
                 </div>
             </div>
-            <div class="col-6">
+            <div class="col-lg-6 col-sm-12 mt-4 mb-3">
                 <img src="https://res.cloudinary.com/sgnolebagabriel/image/upload/v1572635161/startng/Group_227_fzkkii.png" class="img-fluid w-75" style="transform: scaleY(1.3); height: 70vh;">
             </div>
         </div>
@@ -137,4 +177,96 @@
             {!! form::close() !!}
         </div>
     </div>
-@endsection
+    <!-- Footer -->
+    <footer>
+        <div class="container-fluid footer-top col-lg-12">
+            <div class="container col-md-12 p-5">
+                <img class="img-fluid" src="https://res.cloudinary.com/sgnolebagabriel/image/upload/v1572346080/startng/Logo_2_ee1iqv.png">
+                <div class="row">
+                @if(Auth::guest())
+                    <div class="col-md-4 mt-4">
+
+                        <h4 class="">Ready to take the leap?</h4>
+                        <a class="btn btn-success mt-3 pl-5 pr-5"  href="{{route('register')}}">Start</a>
+                    </div>
+                @else
+                     <div class="col-md-4 mt-4">
+
+                         <a class="btn btn-success mt-3 pl-5 pr-5"  href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                         </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                        </form>
+                    </div>
+                @endif
+                    <div class="col-md-2 mt-4">
+
+                        <li><a class="footer-link" href="{{route('about')}}">About Us</a></li>
+                        <li><a class="footer-link" href="{{route('course.index')}}">Our Course</a></li>
+                        <li><a class="footer-link" href="{{route('hire')}}">Hire a Grad</a></li>
+
+                    </div>
+                    <div class="col-md-2 mt-4">
+
+                        <li>
+                            <a class="footer-link" href="{{route('curriculum')}}">Curriculum</a>
+                        </li>
+                        <li>
+                            <a class="footer-link" href="http://lucid.blog/startng">Blog</a>
+                        </li>
+
+                    </div>
+                    <div class="col-md-2 mt-4">
+                        <li>
+                            <a class="footer-link" href="{{route('find-course')}}">Find a Course</a>
+                        </li>
+                        <li>
+                            <a class="footer-link" href="{{route('faq')}}">FAQ</a>
+                        </li>
+                        <li>
+                            <a class="footer-link" href="{{route('contact')}}">Contact Us</a>
+                        </li>
+                    </div>
+                    <div class="col-md-2 mt-4">
+                        <li>
+                            <a class="footer-link" href="{{route('terms')}}">Terms of Service</a>
+                        </li>
+                        <li>
+                            <a class="footer-link" href="{{route('privacy')}}">Privacy Policy</a>
+                        </li>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid footer-social col-lg-12">
+            <div class="container p-3">
+                <div class="clearfix">
+                    <div class="float-left">
+
+                    </div>
+                    <div class="float-right">
+                        <a href="https://twitter.com/hotelsng" class="text-white pl-2 pr-2" style="font-size: 1.5em;"><i
+                                class="fab fa-twitter"></i></a>
+                        <!-- <a href="" class="text-white pl-2 pr-2" style="font-size: 1.5em;"><i
+                                class="fab fa-instagram"></i></a>
+                        <a href="" class="text-white pl-2 pr-2" style="font-size: 1.5em;"><i
+                                class="fab fa-facebook"></i></a> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- End of Footer -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+</body>
+
+</html>
